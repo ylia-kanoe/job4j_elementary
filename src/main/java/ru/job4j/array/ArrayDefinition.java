16e6b519c52b234e4078cb7ec290ad1484a6cdff
Yulia Kh <ylia.kano@gmail.com>
2025-08-06 13:40:46 +0400

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Юля";
+        names[1] = "Миша";
+        names[2] = "Хепи";
+        names[3] = "Флопа";
 
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
