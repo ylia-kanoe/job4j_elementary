0ca7028a3742f67476e4c4ad5cd981f44e97a42e
Yulia Kh <ylia.kano@gmail.com>
2025-08-06 12:43:02 +0400

    Простые числа [#505215]

---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+        int count = 1;
+        for (int index = 2; index <= finish; index++) {
+            if (CheckPrimeNumber.check(index)) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+}
