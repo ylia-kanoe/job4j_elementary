42956c2373b4b2e446ce84ab5a7961b49fe5affc
Yulia Kh <ylia.kano@gmail.com>
2025-08-12 14:55:58 +0400

    PrimeNumber исправлено

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class PrimeNumber {
 
     public static int calc(int finish) {
-        int count = 1;
+        int count = 0;
         for (int index = 2; index <= finish; index++) {
             if (CheckPrimeNumber.check(index)) {
                 count = count + 1;
