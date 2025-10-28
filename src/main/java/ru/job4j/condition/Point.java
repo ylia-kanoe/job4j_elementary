a94eb64d2fc85b9829e7b6ee9f6944f7a38299ac
Yulia Kh <ylia.kano@gmail.com>
2025-10-28 13:15:03 +0400

    Расстояние между точками в трехмерном пространстве

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,35 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
-        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -33,11 +33,39 @@ class PointTest {
     }
 
     @Test
-    void whenPoints0And2And2AndMinus5Then5Dot66() {
+    void whenPoints0And2And2AndMinus5Then7Dot28() {
         Point a = new Point(0, 2);
         Point b = new Point(2, -5);
         double expected = 7.28;
         double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
+
+    @Test
+    void whenPoints0And2And4And2AndMinus5And3Then7Dot34() {
+        Point a = new Point(0, 2, 4);
+        Point b = new Point(2, -5, 3);
+        double expected = 7.28;
+        double output = a.distance(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenPoints4And10And2AndMinus1AndMinus2AndMinus3Then13() {
+        Point a = new Point(4, 10, 2);
+        Point b = new Point(-1, -2, -3);
+        double expected = 13;
+        double output = a.distance(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenPointsMinus2And2And4And1And10And3Then8Dot54() {
+        Point a = new Point(-2, 2, 4);
+        Point b = new Point(1, 10, 3);
+        double expected = 8.54;
+        double output = a.distance(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
 }
